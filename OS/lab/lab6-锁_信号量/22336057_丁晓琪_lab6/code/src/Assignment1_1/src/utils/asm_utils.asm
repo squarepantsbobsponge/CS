e95acfed874b9696b12c7846d7a9a8a8a7cfa3c7
squarepantsbobsponge <120762591+squarepantsbobsponge@users.noreply.github.com>
2024-05-20 20:24:00 +0800

    add

---
+++ OS/lab/lab6-锁_信号量/22336057_丁晓琪_lab6/code/src/Assignment1_1/src/utils/asm_utils.asm
@@ -0,0 +1,210 @@
+[bits 32]
+
+global asm_hello_world
+global asm_lidt
+global asm_unhandled_interrupt
+global asm_halt
+global asm_out_port
+global asm_in_port
+global asm_time_interrupt_handler
+global asm_enable_interrupt
+global asm_enable_interrupt
+global asm_disable_interrupt
+global asm_interrupt_status
+global asm_switch_thread
+global asm_atomic_exchange
+extern c_time_interrupt_handler
+ASM_UNHANDLED_INTERRUPT_INFO db 'Unhandled interrupt happened, halt...'
+                             db 0
+ASM_IDTR dw 0
+         dd 0
+
+; void asm_atomic_exchange(uint32 *register, uint32 *memeory);
+asm_atomic_exchange:
+    push ebp
+    mov ebp, esp
+    pushad
+    ;mov ebx, [ebp + 4 * 2] ; register key
+    ;mov eax, [ebx]         ; 
+    ;mov ebx, [ebp + 4 * 3] ; memory bolt
+    ;xchg [ebx], eax        ;
+    ;mov ebx, [ebp + 4 * 2] ; memory
+    ;mov [ebx], eax  
+    mov ebx, [ebp + 4 * 3] ; memory bolt       ; 
+    lock bts dword[ebx], 0 ;bts bolt的低位，为0锁为空，置1得到锁，为1也置一无影响
+    ;检查bolt原来的位置，也就是检查bts传出来的原值，放在进位标识符CF里，要是为0
+    lahf;提取标志寄存器低八位
+    and ah,1;获取最低位CF寄存器
+    movzx eax, ah ; 将AH中的值零扩展到EAX寄存器（32位） 
+    mov ebx, [ebp + 4 * 2] ; memory
+    mov [ebx], eax   
+    return:
+    popad
+    pop ebp
+    ret
+
+; void asm_switch_thread(PCB *cur, PCB *next);
+asm_switch_thread:
+    push ebp
+    push ebx
+    push edi
+    push esi
+
+    mov eax, [esp + 5 * 4]
+    mov [eax], esp ; 保存当前栈指针到PCB中，以便日后恢复
+
+    mov eax, [esp + 6 * 4]
+    mov esp, [eax] ; 此时栈已经从cur栈切换到next栈
+
+    pop esi
+    pop edi
+    pop ebx
+    pop ebp
+
+    sti
+    ret
+; int asm_interrupt_status();
+asm_interrupt_status:
+    xor eax, eax
+    pushfd
+    pop eax
+    and eax, 0x200
+    ret
+
+; void asm_disable_interrupt();
+asm_disable_interrupt:
+    cli
+    ret
+; void asm_init_page_reg(int *directory);
+
+asm_enable_interrupt:
+    sti
+    ret
+asm_time_interrupt_handler:
+    pushad
+    
+    ; 发送EOI消息，否则下一次中断不发生
+    mov al, 0x20
+    out 0x20, al
+    out 0xa0, al
+    
+    call c_time_interrupt_handler
+
+    popad
+    iret
+
+; void asm_in_port(uint16 port, uint8 *value)
+asm_in_port:
+    push ebp
+    mov ebp, esp
+
+    push edx
+    push eax
+    push ebx
+
+    xor eax, eax
+    mov edx, [ebp + 4 * 2] ; port
+    mov ebx, [ebp + 4 * 3] ; *value
+
+    in al, dx
+    mov [ebx], al
+
+    pop ebx
+    pop eax
+    pop edx
+    pop ebp
+    ret
+
+; void asm_out_port(uint16 port, uint8 value)
+asm_out_port:
+    push ebp
+    mov ebp, esp
+
+    push edx
+    push eax
+
+    mov edx, [ebp + 4 * 2] ; port
+    mov eax, [ebp + 4 * 3] ; value
+    out dx, al
+    
+    pop eax
+    pop edx
+    pop ebp
+    ret
+
+; void asm_unhandled_interrupt()
+asm_unhandled_interrupt:
+    cli
+    mov esi, ASM_UNHANDLED_INTERRUPT_INFO
+    xor ebx, ebx
+    mov ah, 0x03
+.output_information:
+    cmp byte[esi], 0
+    je .end
+    mov al, byte[esi]
+    mov word[gs:bx], ax
+    inc esi
+    add ebx, 2
+    jmp .output_information
+.end:
+    jmp $
+
+; void asm_lidt(uint32 start, uint16 limit)
+asm_lidt:
+    push ebp
+    mov ebp, esp
+    push eax
+
+    mov eax, [ebp + 4 * 3]
+    mov [ASM_IDTR], ax
+    mov eax, [ebp + 4 * 2]
+    mov [ASM_IDTR + 2], eax
+    lidt [ASM_IDTR]
+
+    pop eax
+    pop ebp
+    ret
+
+asm_hello_world:
+    push eax
+    xor eax, eax
+
+    mov ah, 0x03 ;青色
+    mov al, 'H'
+    mov [gs:2 * 0], ax
+
+    mov al, 'e'
+    mov [gs:2 * 1], ax
+
+    mov al, 'l'
+    mov [gs:2 * 2], ax
+
+    mov al, 'l'
+    mov [gs:2 * 3], ax
+
+    mov al, 'o'
+    mov [gs:2 * 4], ax
+
+    mov al, ' '
+    mov [gs:2 * 5], ax
+
+    mov al, 'W'
+    mov [gs:2 * 6], ax
+
+    mov al, 'o'
+    mov [gs:2 * 7], ax
+
+    mov al, 'r'
+    mov [gs:2 * 8], ax
+
+    mov al, 'l'
+    mov [gs:2 * 9], ax
+
+    mov al, 'd'
+    mov [gs:2 * 10], ax
+
+    pop eax
+    ret
+
+asm_halt:
+    jmp $
