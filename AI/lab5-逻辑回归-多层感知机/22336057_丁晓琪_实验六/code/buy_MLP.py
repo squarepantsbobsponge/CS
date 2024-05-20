6279ec3c16e5aa8c69c0be977b80782b6fa514c8
squarepantsbobsponge <120762591+squarepantsbobsponge@users.noreply.github.com>
2024-05-20 20:22:01 +0800

    add

---
+++ AI/lab5-逻辑回归-多层感知机/22336057_丁晓琪_实验六/code/buy_MLP.py
@@ -0,0 +1,153 @@
+import string 
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation  
+import csv
+
+class LR:
+    def __init__(self,filename:string) :#读取数据，整理数据，设置参数初始值和参数数量
+        #提取信息
+        f=open(filename)
+        csv_reader=csv.reader(f)
+        self.data=[]#存储提取出来的数据，格式为【【A，E，P】, ....】
+        flag=1#去掉头信息
+        self.lable=[]#数据标准化
+        self.x1_x2=[]
+        max_x1=0
+        max_x2=0
+        min_x1=10000000
+        min_x2=10000000       
+        for line in csv_reader:
+            if flag==1:
+                flag=0
+                continue
+            int_list=[int(s) for s in line]
+            self.data.append(int_list)
+            #这里调整一下y的取值 标签0改为标签-1
+            if int_list[2]==0:
+                int_list[2]=-1
+            self.lable.append(int_list[2])
+            tmp=[]
+            tmp.append(1)
+            tmp.append(int_list[0])
+            tmp.append(int_list[1])
+            self.x1_x2.append(tmp)
+            if(tmp[1]>max_x1):
+                max_x1=tmp[1]
+            if(tmp[1]<min_x1):
+                min_x1=tmp[1]
+            if(tmp[2]>max_x2):
+                max_x2=tmp[2]
+            if(tmp[2]<min_x2):
+                min_x2=tmp[2]
+        #标准化
+        for i in range(0,len(self.x1_x2)):
+            self.x1_x2[i][1]=(self.x1_x2[i][1]-min_x1)/(max_x1-min_x1)
+            self.x1_x2[i][2]=(self.x1_x2[i][2]-min_x2)/(max_x2-min_x2)            
+       #设置参数量，三个参数a+bx1+cx2
+        self.arg_num=3
+        #设置初始参数 #初始化为1
+        self.arg=[]
+        self.arg.append(1)
+        self.arg.append(1)
+        self.arg.append(1)
+        #设置学习步长
+        self.alpha=0.06 #要逐步减小步长，先固定#0.5步长太大了只会震荡  ##0.05的时候0.788
+        #记录代价，画图分析用
+        self.loss_list=[]
+        
+    def Hypothesis_Function(self,index:int):#计算多层感知机的的假设值，index是要计算的数据的标号
+        #直接运算,第一个参数0次的，偏置因子不要乘特征  单层感知机是sign函数不是sigmod函数
+        z=self.arg[0]+self.x1_x2[index][1]*self.arg[1]+self.x1_x2[index][2]*self.arg[2]
+        if z>=0: #对sigmoid函数优化，避免出现极大的数据溢出
+            return 1
+        else:
+            return -1
+
+    
+    def Cost(self,index):#单个data损失计算
+       # theta=np.matrix(self.arg)
+       y=self.lable[index]
+       ret=-y*(self.arg[0]+self.x1_x2[index][1]*self.arg[1]+self.x1_x2[index][2]*self.arg[2])
+       return ret
+    
+    def Loss(self):#计算当前模型所有数据的预估和真实之间的误差之和
+        num=len(self.data)
+        sum=0
+        count=0
+        ##只有分类错误的才能算代价
+        for i in range(0,num):
+            if self.Cost(i)>=0:
+                sum+=self.Cost(i)
+                count+=1 #误分类的总数
+        return sum/count 
+
+    
+    def Gradient_Descent(self): 
+        for index in range(0,len(self.lable)):
+            if self.Cost(index)>=0:
+                #cost>=0是误分类点，更新参数   #随机梯度下降法，只有误分类点才能更新参数
+                for i in range(0,self.arg_num):
+                    if i==0:
+                        self.arg[i]=self.arg[i]+self.alpha*self.lable[index]
+                    else:
+                        self.arg[i]=self.arg[i]+self.alpha*self.lable[index]*self.x1_x2[index][i] 
+        
+
+    def Iterate(self,time):#参数的迭代更新和每代的误差的获得
+        for i in range(0,time):
+            self.loss_list.append(self.Loss())
+            self.Gradient_Descent()#更新所有参数
+        #计算正确率arg[0]+arg[1]x1+ arg[2]x2>0 1小于0就是0
+        sum=0
+        for i in range(0,len(self.lable)):
+            tm=self.Hypothesis_Function(i)
+            if(tm>=0 and self.lable[i]==1):
+                sum+=1
+            elif(tm<0 and self.lable[i]==-1):
+                sum+=1
+        print("正确率：",sum/len(self.lable))
+    def showgraph(self):##最终答案
+             fig2=plt.figure(2)
+             x1_0=[]  #存放标签为0的特征值1
+             x2_0=[]  
+             x1_1=[]  #存放标签为1的特征值1
+             x2_1=[]
+             for i in range(0,len(self.data)):
+                 if(self.lable[i]==-1):
+                    x1_0.append(self.x1_x2[i][1])
+                    x2_0.append(self.x1_x2[i][2])
+                 else:
+                    x1_1.append(self.x1_x2[i][1])
+                    x2_1.append(self.x1_x2[i][2])    
+             #print(x1_1)
+             plt.scatter(x1_0, x2_0, marker='.', color='red')
+             plt.scatter(x1_1, x2_1, marker='.', color='blue')       
+             plt.xlabel("x1")
+             plt.ylabel("x2")  
+                #画直线arg[0]+arg[1]x1+ arg[2]x2=0
+             x1_values = np.linspace(0, 1,30)  # 替换起始值、结束值和点数为你需要的值  ##随机生成30个值给x1
+             print("参数为：",self.arg)
+             x2_values = (0- self.arg[0] - self.arg[1]*x1_values) / self.arg[2] #根据生成的x1按照模型计算x2的值 
+             plt.plot(x1_values, x2_values, color='pink')  
+             #Loss分析
+             fig3=plt.figure(3)
+             y=self.loss_list
+             x=list(range(len(y)))
+            #  for i in range(0,len(x)):  
+            #    x[i] *= 10 
+            #  print(x)
+            #  print(y)
+             plt.scatter(x, y, marker='.', color='red')
+             plt.xlabel("time")
+             plt.ylabel("Loss") 
+             plt.show()
+def main():
+    print("迭代次数为3000")                
+    filename="./data.csv"
+    LR_classify=LR(filename)
+    LR_classify.Iterate(3000)
+    LR_classify.showgraph()
+
+if __name__=="__main__":
+    main()
---
+++ AI/lab7-决策树/Decision_Tree.py
@@ -0,0 +1,188 @@
+import string 
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation  
+import math
+import csv
+##多叉树的数据结构
+class Node:
+    def __init__(self,value,data):#value是父节点分支属性下自己的类别是什么
+        self.value=value
+        self.children=[]#子节点
+        self.sort=None #下一个要分支的属性
+        #要不要往里面放data呢
+        self.data=data
+        self.ent=0 #当层信息熵
+        self.leave=-1 #0为0类，1为1类，-1为分支
+class D_Tree:
+    def __init__(self,filename:string) :#读取数据，整理数据，设置参数初始值和参数数量
+        #提取信息
+        f=open(filename)
+        csv_reader=csv.reader(f)
+        self.data_all=[]#存储提取出来的数据，格式为【【A，E，P】, ....】
+        flag=1#去掉头信息
+        attribute_1=[]##属性集 [[]]
+        attribute_2=[]##第一位代表在data中的index
+        attribute_3=[]
+        attribute_4=[]
+        for line in csv_reader:
+            if flag==1:
+                flag=0
+                continue
+            #提取整理数据 #不将数据数字化了，直接上，避免测试集还要处理
+            for i in range(0,len(line)):
+                if(i==0 and line[i] not in attribute_1):
+                    attribute_1.append(line[i])
+                if(i==1 and line[i] not in attribute_2):
+                    attribute_2.append(line[i])
+                if(i==2):#对标签处理以下
+                    line[i]=int(line[i])
+                    if(line[i]<=30000):
+                        line[i]=0
+                    else:
+                        line[i]=1 #标签变成0，1
+                if(i==3):#字符串转数字
+                    line[i]=int(line[i])//10   
+                    if(line[i] not in attribute_3): 
+                        attribute_3.append(line[i]) ##这里要离散化处理  每10年一个单位
+                if(i==4 and line[i] not in attribute_4):
+                    attribute_4.append(line[i])   
+            self.data_all.append(line)
+            self.data=self.data_all[:500]
+           # print(self.data)   
+        self.attribute=[attribute_1,attribute_2,[],attribute_3,attribute_4]   #空一位后续便于直接按照[]索引对照data[]索引                 
+        self.head=self.Tree_Generate(self.data,self.attribute,0,[])
+   
+    def Ent(self,data):#信息熵的计算
+        sum_1=0
+        sum_0=0 #统计标签数
+        for i in range(0,len(data)):
+            if(data[i][2]==0):
+                sum_0+=1
+            else:
+                sum_1+=1
+        ##统计概率
+        p_1=sum_1/(sum_1+sum_0)
+        p_0=sum_0/(sum_0+sum_1)
+        ##计算熵
+        if p_1==1 or p_0==1:
+            ent=0  #熵为0 令算，不要走对数
+        else:
+            ent=-p_1*math.log2(p_1)-p_0*math.log2(p_0)
+        return ent,sum_1,sum_0
+    
+    def compute_IGR(self,attribute,node:Node,no_attribute):#计算属性的信息增益率
+        #先建立个属性取值字典，key是该属性的具体取值，value是[0,0，0]该取值的总数，和该取值下，0和1的个体数
+        ##感觉可以一起求，少遍历
+        #初始化完成
+        data=node.data
+        cur_ent=node.ent
+        best_IGR=-1000 #最好的信息增益率
+        best_attri=-1
+        dict=[{},{},{},{},{}]#空一个便于索引
+        for i in range(0,len(attribute)):
+            if i==2 :continue
+            for j in attribute[i]:
+                dict[i][j]=[0,0,0]
+        ##遍历数据，整理数据
+        for data_singel in data:
+            for i in range(0,5):
+                if i==2:continue
+                dict[i][data_singel[i]][0]+=1
+                if data_singel[2]==0:#分类标签为0
+                    dict[i][data_singel[i]][1]+=1
+                else:
+                    dict[i][data_singel[i]][2]+=1
+        ##算每个属性的信息增益率
+        for i in range(0,len(dict)):
+            if i==2 or i in no_attribute:continue
+            dict_index=dict[i]
+            count_ent=0
+            IV=0
+            #先算信息增益
+            sum=len(data) #数据总数
+            for key,item in dict_index.items(): #属性下的单类别
+                ##要是标签全部一致，熵为0，不要用对数会有对数错误
+                p=item[0]/sum  ##这里还有点问题，万一人家没有这个分类，但是下面解决了，这个分类里面要是没有样本，就选父节点最多的标签
+                if item[1]==0 or item[2]==0:
+                    d_v_ent=0
+                else:
+                    p_0=item[1]/item[0]
+                    p_1=item[2]/item[0] 
+                    d_v_ent=-p_0*math.log2(p_0)-p_1*math.log2(p_1)
+                count_ent+=(p*d_v_ent)
+                d_v_IV=p*math.log2(p)
+                IV-=d_v_IV
+            Gain=cur_ent-count_ent
+            IGR=Gain/IV
+            if(IGR>=best_IGR):
+                best_IGR=IGR
+                best_attri=i
+        node.sort=best_attri
+        return best_attri
+    def Tree_Generate (self,data,attribute,value,no_attribute):
+        node=Node(value,data)
+        #当层信息熵计算
+        node.ent,sum_1,sum_0=self.Ent(data)
+        #终止条件1:如果data中的数据全部属于同一类别，那直接标记为C类叶子节点
+        #终止条件2:data在所有属性上取值相同
+        flag1=data[0][2] #兼职代表
+        flag2=data[0]
+        flag=1
+        for i in range(1,len(data)):
+            if data[i][2]!=flag1:
+                flag1=data[i][2]
+            for j in range(0,5):#判断属性是否从一而终
+                if(flag2[j]!=data[i][j] and j!=2):
+                    flag=0
+            if(flag1!=data[0][2] and flag==0):
+                    break
+        if(flag1==data[0][2]):##从头到尾的标签都一样，return为叶子节点
+            node.leave=flag1
+            return node
+        if(flag==1):##是否所有属性全部相同
+            node.leave=1 if sum_1>sum_0 else 0
+            return node
+        ##挑选最佳分类属性
+        a_best=self.compute_IGR(attribute,node,no_attribute) #返回分类属性的索引
+        ##按照最佳属性分支
+        #先把data分好
+        a_sort={} #键为取值，value为样本
+        #初始化
+        for atrri in attribute[a_best]:
+            a_sort[atrri]=[]
+        #分数据
+        for data_index in data:
+            a_sort[data_index[a_best]].append(data_index)
+        #分支
+        for atrri in attribute[a_best]:
+            d_v=a_sort[atrri]
+            if(len(d_v)==0):
+                child_node=Node(0,data)
+                child_node.leave=1 if sum_1>sum_0 else 0
+                node.children.append(child_node)
+            else:
+                ##要是attribute的子集，但是前面都直接拿它当索引了
+                no_attribute.append(a_best)
+                child_node=self.Tree_Generate(d_v,attribute,atrri,no_attribute)
+                node.children.append(child_node)
+                no_attribute.remove(a_best) #这是递归式的
+        return node 
+    def find(self,node:Node,data_index):
+        if(node.leave!=-1):
+            return node.leave
+        a_best=node.sort
+        for next_node in node.children:
+            if(next_node.value==data_index[a_best]):
+                return self.find(next_node,data_index)
+    def test(self):
+        sum=0
+        for i in range(400,600):
+           data_index=self.data_all[i]
+           test_node=self.head
+           test_label=self.find(test_node,data_index)
+           if data_index[2]==test_label:
+               sum+=1
+        print("正确率",sum/200)          
+d_tree=D_Tree("./DT_data.csv")
+d_tree.test()
